305c3d8255439ca081faa38df718819a1a5b547c
jobanpreet <joban52@gmail.com>
2022-01-04 14:36:44 -0600

    Added GeoLocation to Fetch  Stops

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { useState } from "react";
 import Stops from "../components/Stops/Stops";
-
-const defaultLoc = "49.89242728391351--97.14950606321388";
+import GetLocation from "../components/Utils/GetLocation";
+const defaultCoords = "49.89242728391351--97.14950606321388";
 const serverUrl = "https://winnipegtransitserver.herokuapp.com/api/";
 
 function Home() {
+  const [coords, setCoords] = useState("");
+  GetLocation(setCoords);
   return (
     <div className="container">
-      <Stops url={serverUrl + defaultLoc} />
+      {coords ? (
+        <Stops url={serverUrl + coords} />
+      ) : (
+        <>
+          <h2>Allow Location to See nearby Stops</h2>
+          <h2>Deny if Out of Winnipeg</h2>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Stops/Stop.js
+++ src/components/Stops/Stop.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Stop(props) {
+  const address = "/stop/" + props.stop.key;
   return (
-    <div id={props.stop.key} className="stop">
-      <h3>{props.stop.name}</h3>
-      <h3>{props.stop.distances.direct}</h3>
-    </div>
+    <Link to={address}>
+      <div id={props.stop.key} className="stop">
+        <h3>{props.stop.name}</h3>
+        <h3>{props.stop.distances.direct}m</h3>
+      </div>
+    </Link>
   );
 }
 
