104a32b1e366fb138c8a4a5e47c09dd407a71ebe
jobanpreet <joban52@gmail.com>
2022-01-09 00:08:54 -0600

    Added bus routes for each stop

--- src/components/Stops/Stop.js
+++ src/components/Stops/Stop.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useFetch from "./../Utils/useFetch";
 
 function Stop(props) {
   const address = "/stop/" + props.stop.key;
+  const FetchRoutes = (key) => {
+    const { data, loading } = useFetch(
+      "https://winnipegtransitserver.herokuapp.com/api/stop-routes/" + key
+    );
+    return (
+      <div>
+        {!loading ? (
+          data.map((stop) => {
+            return stop["number"] + " ";
+          })
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  };
   return (
     <li className="stop">
       <Link to={address} className="stop_link">
         <div className="stop_info">
           <h5>{props.stop.name}</h5>
-          <h6 className="stop_buses">Buses: 1 , 2 , 3</h6>
+          <h6 className="stop_buses">{FetchRoutes(props.stop.key)}</h6>
         </div>
         <div className="stop_distance">
           <h6>{props.stop.distances.direct}m</h6>
