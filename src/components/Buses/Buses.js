92d0ed0e9eb794fe4fc5381a37907a587ac60da8
jobanpreet <joban52@gmail.com>
2022-02-04 23:54:00 -0600

    Added Loader for homepage and schedule page

--- src/components/Buses/Buses.js
+++ src/components/Buses/Buses.js
@@ -2,11 +2,11 @@ import React from "react";
 import Bus from "./Bus";
 function Buses(props) {
   return (
-    <>
+    <div className="bus_list">
       {props.data.map(function (bus) {
         return <Bus bus={bus} />;
       })}
-    </>
+    </div>
   );
 }
 
--- src/components/Stops/Stops.js
+++ src/components/Stops/Stops.js
@@ -2,10 +2,11 @@ import React from "react";
 import useFetch from "../Utils/useFetch";
 import "./stops.css";
 import DisplayStops from "./DisplayStops";
+import Loader from "../Utils/Loader";
 
 function Stops(props) {
   const { data, loading } = useFetch(props.url);
   if (!loading) return <DisplayStops data={data} />;
-  else return <h2>Loading</h2>;
+  else return <Loader />;
 }
 export default Stops;
